069fce6b4c75140211db73ac5af23986393366cd
10mone <tomone.nkmr24@gmail.com>
2019-07-15 21:48:33 +0900

    botton1の実装

--- CarnivorousGame/ViewController.swift
+++ CarnivorousGame/ViewController.swift
@@ -20,54 +20,54 @@ class ViewController: UIViewController {
     @IBAction func button(_ sender: UIButton) {
         PushButton()
         sender.isHidden = true
-        BlinkFlying(sender)
+        //BlinkFlying(sender)
     }
     
     @IBAction func button2(_ sender: UIButton) {
         PushButton()
         sender.isHidden = true
-        BlinkFlying(sender)
+        //BlinkFlying(sender)
     }
     @IBAction func button3(_ sender: UIButton) {
         PushButton()
         sender.isHidden = true
-        BlinkFlying(sender)
+        //BlinkFlying(sender)
     }
     @IBAction func button4(_ sender: UIButton) {
         PushButton()
         sender.isHidden = true
-        BlinkFlying(sender)
+        //BlinkFlying(sender)
     }
     @IBAction func button5(_ sender: UIButton) {
         PushButton()
         sender.isHidden = true
-        BlinkFlying(sender)
+        //BlinkFlying(sender)
     }
     @IBAction func button6(_ sender: UIButton) {
         PushButton()
         sender.isHidden = true
-        BlinkFlying(sender)
+        //BlinkFlying(sender)
     }
     @IBAction func button7(_ sender: UIButton) {
         PushButton()
         sender.isHidden = true
-        BlinkFlying(sender)
+        //BlinkFlying(sender)
     }
     @IBAction func button8(_ sender: UIButton) {
         PushButton()
         sender.isHidden = true
-        BlinkFlying(sender)
+        //BlinkFlying(sender)
     }
     @IBAction func button9(_ sender: UIButton) {
         PushButton()
         sender.isHidden = true
-        BlinkFlying(sender)
+        //BlinkFlying(sender)
     }
     //蝶々押したら減点！
     @IBAction func cyou_button(_ sender: UIButton) {
-        if score_sum > 3{
+        if score_sum > 3 {
             score_sum -= 3
-        }else{
+        } else {
             score_sum = 0
         }
         score.text = score_sum.description
@@ -84,14 +84,18 @@ class ViewController: UIViewController {
     
     //消えた蚊を再び表示するまでの時間
     func BlinkFlying(_ sender: UIButton){
-        timer = Timer.scheduledTimer(withTimeInterval: 2, repeats: true, block: {_ in sender.isHidden = false})
-        print("blinkflying")
-        //sender.isHidden = false
-        //timer = Timer.scheduledTimer(withTimeInterval: 2, repeats: true, block: {_ in sender.isHidden = true})
+//        timer = Timer.scheduledTimer(withTimeInterval: 2, repeats: true, block: {_ in sender.isHidden = false})
+
+        sender.isHidden = false
+        timer = Timer.scheduledTimer(withTimeInterval: 2, repeats: false, block: {_ in sender.isHidden = true})
     }
     
     //合計スコアを渡して画面遷移
     func changeView(){
+        //メモリ解放
+        if let workingTimer = timer{
+            workingTimer.invalidate()
+        }
         //description追加
         self.performSegue(withIdentifier: "toLastPage", sender: score_sum.description)
     }
@@ -99,11 +103,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        timer = Timer.scheduledTimer(withTimeInterval: 3.0, repeats: true, block: {_ in self.BlinkFlying(self.button)})
         
         timer = Timer.scheduledTimer(withTimeInterval: 30.0, repeats: false, block: {_ in self.changeView()})
-        //timer = Timer.scheduledTimer(timeInterval: 10.0, target: self, selector: #selector(ViewController.changeView), userInfo: nil, repeats: false)
-    }
-
+        }
+    
     
     //Segueの初期化をするメソッドをオーバーライド
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -112,12 +116,18 @@ class ViewController: UIViewController {
             LastViewController.finally_score = sender as! String
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    /*
+    メモリが解放されているか確認
+    deinit{
+        print("release")
+    }
+    */
+    
 }
 
