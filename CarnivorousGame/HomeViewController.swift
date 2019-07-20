5c819fe8ab1b9dcbdd4d13c090c372279f388522
10mone <tomone.nkmr24@gmail.com>
2019-07-20 15:48:58 +0900

    トップページ完成！

---
+++ CarnivorousGame/HomeViewController.swift
@@ -0,0 +1,9 @@
+//
+//  HomeViewController.swift
+//  CarnivorousGame
+//
+//  Created by Nakamura Tomone on 2019/07/20.
+//  Copyright © 2019年 Nakamura Tomone. All rights reserved.
+//
+
+import Foundation
